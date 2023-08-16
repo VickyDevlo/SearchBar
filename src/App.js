f24cfb107c89a481da07b6dc264aad9df08277b3
Pranav Magare <vickymagare1996@gmail.com>
2023-08-16 13:19:08 +0530

    commit

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import { SearchBar } from "./Components/SearchBar";
 function App() {
 
   const [result, setResult] = useState([]);
-  
+
   return (
     <div className="App">
       <SearchBar result={result} setResult={setResult} />
--- src/Components/SearchBar.js
+++ src/Components/SearchBar.js
@@ -3,53 +3,51 @@ import "./SearchBar.css";
 import { FaSearch } from "react-icons/fa";
 
 export const SearchBar = ({ result, setResult }) => {
+    const [input, setInput] = useState("");
 
-  const [input, setInput] = useState("");
+    const fetchData = (value) => {
+        fetch("https://jsonplaceholder.typicode.com/users")
+            .then((response) => response.json())
+            .then((json) => {
+                const results = json.filter((user) => {
+                    return (
+                        value &&
+                        user &&
+                        user.name &&
+                        user.name.toLowerCase().includes(value)
+                    );
+                });
+                setResult(results);
+            });
+    };
 
-  const fetchData = (value) => {
-    fetch("https://jsonplaceholder.typicode.com/users")
-      .then((res) => res.json())
-      .then((json) => {
-        const results = json.filter((user) => {
-          return (
-            value &&
-            user &&
-            user.name &&
-            user.name.toLowerCase().includes(value)
-          );
-        });
-        setResult(results);
-      });
-  };
+    const handlerChange = (e) => {
+        const { value } = e.target;
+        setInput(value);
+        fetchData(value);
+    };
 
-  const handlerChange = (e) => {
-    const { value } = e.target;
-
-    setInput(value);
-    fetchData(value);
-  };
-
-  return (
-    <div className="serach_bar_container">
-      <div className="input_container">
-        <FaSearch id="serach_icon" />
-        <input
-          type="text"
-          placeholder="Type to serach..."
-          value={input}
-          onChange={handlerChange}
-        />
-      </div>
-
-      <div className="result_list">
-        {result.map((data, i) => {
-          return (
-            <div key={i} className="search_result">
-              {data.name}
+    return (
+        <div className="serach_bar_container">
+            <div className="input_container">
+                <FaSearch id="serach_icon" />
+                <input
+                    type="text"
+                    placeholder="Type to serach..."
+                    value={input}
+                    onChange={handlerChange}
+                />
             </div>
-          );
-        })}
-      </div>
-    </div>
-  );
+
+            <div className="result_list">
+                {result.map((data, i) => {
+                    return (
+                        <div key={i} className="search_result">
+                            {data.name}
+                        </div>
+                    );
+                })}
+            </div>
+        </div>
+    );
 };
